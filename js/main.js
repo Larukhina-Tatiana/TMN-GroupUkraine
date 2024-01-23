d69ae06faec245c10aeb48f0da099855049dc3ad
Larukhina-Tatiana <ltasya72@gmail.com>
2024-01-23 14:11:32 +0200

    свёрстан header

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ $(".nav__linkbtn-arrow").on("click", function () {
   $(".nav__linkbtn-arrow").toggleClass("active");
   $(".nav__submenu-list").toggleClass("active");
 });
+
+$(".phone__arrow").on("click", function () {
+  $(".phone__arrow").toggleClass("active");
+  $(".phone__list").toggleClass("active");
+});
