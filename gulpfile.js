b9d09fe110b71213f9caae68b64c3a22c67e0a70
Larukhina-Tatiana <ltasya72@gmail.com>
2024-01-31 11:57:20 +0200

    Закончена hero

--- gulpfile.js
+++ gulpfile.js
@@ -95,20 +95,20 @@ function scripts() {
 function images() {
   // return src(["app/images/**/*.*", "!app/images/**/*.svg"])
   return (
-    src(["images/src/comments/*.*", "!images/src/*.svg"])
+    src(["images/src/hero/*.*", "!images/src/*.svg"])
       // .pipe(newer("app/images"))
       // .pipe(avif({ quality: 50 }))
 
       // .pipe(src("app/images/**/*.*"))
-      .pipe(src("images/src/comments/*.*"))
+      .pipe(src("images/src/hero/*.*"))
       .pipe(newer("images"))
       .pipe(webp())
 
-      .pipe(src("images/src/comments/*.*"))
+      .pipe(src("images/src/hero/*.*"))
       .pipe(newer("images"))
       .pipe(imagemin())
 
-      .pipe(dest("images/comments/"))
+      .pipe(dest("images/hero/"))
   );
 }
 
