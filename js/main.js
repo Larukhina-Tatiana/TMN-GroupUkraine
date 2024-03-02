e3ab2dc8a0d91975759459965893d3c8148d932d
Larukhina-Tatiana <ltasya72@gmail.com>
2024-03-02 13:09:42 +0200

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -34,6 +34,57 @@ if ($(window).width() < 890.99) {
     }
   })();
 
+  const swiper = new Swiper(".popular__slider", {
+    loop: true,
+    observer: true,
+    observeParents: true,
+    watchOverflow: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    // speed: 800,
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".popular__slider-dotts",
+      clickable: true,
+      dinamicBullets: true,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        // spaceBetween: 40,
+        // autoHeight: true,
+      },
+      // breakpoints: {
+      //   320: {
+      //     slidesPerView: 1,
+      //     // spaceBetween: 40,
+      //     // autoHeight: true,
+      //   },
+
+      600: {
+        slidesPerView: 2,
+      },
+
+      885: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        // autoHeight: true,
+      },
+      on: {
+        init: function (swiper) {},
+      },
+    },
+  });
+
   const btnSort = document.querySelector(".menu__sort");
   const aside = document.querySelector(".aside");
 
@@ -42,57 +93,6 @@ if ($(window).width() < 890.99) {
     btnSort.classList.toggle("open");
   });
 }
-
-const swiper = new Swiper(".popular__slider", {
-  loop: true,
-  observer: true,
-  observeParents: true,
-  watchOverflow: true,
-  slidesPerView: 4,
-  spaceBetween: 30,
-  // speed: 800,
-  // autoplay: {
-  //   delay: 3000,
-  //   disableOnInteraction: false,
-  // },
-  pagination: {
-    el: ".popular__slider-dotts",
-    clickable: true,
-    dinamicBullets: true,
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-      // spaceBetween: 40,
-      // autoHeight: true,
-    },
-    // breakpoints: {
-    //   320: {
-    //     slidesPerView: 1,
-    //     // spaceBetween: 40,
-    //     // autoHeight: true,
-    //   },
-
-    600: {
-      slidesPerView: 2,
-    },
-
-    885: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      // autoHeight: true,
-    },
-    on: {
-      init: function (swiper) {},
-    },
-  },
-});
 
 // ==================================
 // Counter
